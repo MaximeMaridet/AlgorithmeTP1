9a79f028cd35a1024631b606a9271bba097862dc
MaximeMaridet <maxime-maridet@hotmail.com>
2020-01-21 12:39:36 +0100

    Système de choix aléatoire monstre
    Permet au monstre de choisir aléatoirement entre un mode défense/attaque

--- combat.c
+++ combat.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 	int PlayerHP = 100;
-	int MonsterHP = 140;
+	int MonstreHP = 140;
 	int tour = 1;
 	int monstreDmg = 15;
 	int playerDmg = 30;
 	int playerAction = 0;
 	int damage = 0;
+	int monstreAction = 0;
 	
 	
-	while (MonsterHP>0 && PlayerHP>0) {
+	while (MonstreHP>0 && PlayerHP>0) {
 		printf("Tour %d\n", tour);
 		
 		printf("(1) Attaquer (2) Defendre\n");
@@ -18,26 +20,35 @@ int main() {
 			scanf("%d", &playerAction);
 			printf("%d\n", playerAction);
 		}
+		monstreAction = rand()%2+1;
 		
 		if (playerAction == 1) {
-			printf("HP Monstre:%d\n", MonsterHP);
-			MonsterHP -= playerDmg;
-			printf("Le monstre subit %d points de degats\n", playerDmg);
-			printf("HP Monstre:%d\n", MonsterHP);
+				printf("HP Monstre:%d\n", MonstreHP);
+			
+			if (monstreAction == 2) {
+				damage = playerDmg/4;
+			} else {
+				damage = playerDmg;
+			}
+			MonstreHP -= damage;
+			printf("Le monstre subit %d points de degats\n", damage);
+			printf("HP Monstre:%d\n", MonstreHP);
 		} else if (playerAction == 2) {
 			printf("Vous vous recroquevillez en position defensive\n");
 		}
 		
-		if (MonsterHP >0) {
+		if (MonstreHP >0) {
 			printf("Hp Joueur : %d\n", PlayerHP);
+			if (monstreAction == 1) {
 				if (playerAction == 2) {
 					damage = monstreDmg/4;
 				} else {
 					damage = monstreDmg;
-				}
-				PlayerHP -= damage;
+			}
+				PlayerHP -= damage; 
 				printf("Vous subissez %d points de degats\n", damage);
 			printf("Hp Joueur: %d\n", PlayerHP);
+			}
 		}
 		printf("#############################\n");
 		tour += 1;
