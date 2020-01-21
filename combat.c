728caba99c0f0b757ef9575b78570563a817ab92
MaximeMaridet <maxime-maridet@hotmail.com>
2020-01-21 10:45:16 +0100

    Combat Entier
    Ajout compteur de tour
    Ajout séparation entre les tours

--- combat.c
+++ combat.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 int main() {
-	int MonsterHP = 345;
+	int tour = 1;
+	int MonsterHP = 160;
 	int damage = 20;
 	
-	printf("HP:%d\n", MonsterHP);
-	MonsterHP -= damage;
-	printf("Vous subissez %d points de degats\n", damage);
-	printf("HP:%d\n", MonsterHP);
+	while (MonsterHP>0) {
+		printf("Tour %d\n", tour);
+		printf("HP:%d\n", MonsterHP);
+		MonsterHP -= damage;
+		printf("Vous subissez %d points de degats\n", damage);
+		printf("HP:%d\n", MonsterHP);
+		
+		printf("#############################\n");
+		tour += 1;
+	}
 	
 	return 0;
 }
